1063bb8ad0677dcab7adc3dd5513c407595ac6e6
adityaundre <adityarundre@gmail.com>
2023-12-30 19:13:10 +0530

    Section toggle false

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,13 +5,18 @@ import headphones from "../../assets/vibrating-headphone 1.png"
 const Hero=()=>{
     return(
         <div className={styles.hero}>
-            
+            <div>
 
                 <h1>100 Thousand songs, ad-free</h1>
+                
+                    
                 <h1>Over thousands podcast episodes</h1>
+                
+            </div>
+                
 
             
-            
+
                 <img src={headphones} alt="headphones" width={112}/>
                 
             
--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -5,7 +5,7 @@ import { CircularProgress } from "@mui/material";
 import Card from "../Card/Card";
 
 const Section=({title, data, type})=>{
-    const[carouselToggle, setCarouselToggle]=useState(true);
+    const[carouselToggle, setCarouselToggle]=useState(false);
 
     const handleToggle=()=>{
         setCarouselToggle((preState)=>!preState)
