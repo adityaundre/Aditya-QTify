3e157e3889df4124d5880c47e51d93a0f7b3c8cb
adityaundre <adityarundre@gmail.com>
2024-01-01 16:33:31 +0530

    section show all

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -6,7 +6,7 @@ import Card from "../Card/Card";
 import Carousel from "../Carousel/Carousel";
 
 const Section=({title, data,type})=>{
-    const[carouselToggle, setCarouselToggle]=useState(false);
+    const[carouselToggle, setCarouselToggle]=useState(true);
 
     const handleToggle=()=>{
         setCarouselToggle((preState)=>!preState)
